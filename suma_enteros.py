c23fb2ea2721fc1d24be8f616ba92ab2adc46126
lauraq5 <lauraquintana@colegioguanenta.edu.co>
2023-04-28 10:10:24 -0500

    se modifico el archivo

--- suma_enteros.py
+++ suma_enteros.py
@@ -2,6 +2,7 @@
 # Desktop app No. 1
 #------------------------
 
+
 # se importa la libreria tkinter con todas sus funciones
 from tkinter import *
 
@@ -47,24 +48,39 @@ titulo.place(x=240,y=10)
 
 # etiqueta para valor de x
 lb_x = Label(frame_entrada, text = "x = " )
-lb_x.config(bg="white, fg="blue", font=("Helvetica", 18))
-lb_x.Place(x=240, y=60)
+lb_x.config(bg="white", fg="blue", font=("Helvetica", 18))
+lb_x.place(x=240, y=60)
 
 # caja de texto para valor x
 Entry_x = Entry(frame_entrada)
-Entry_x.config(bg="white", fg="blue, font=("Times New Roman", 18))
-Entry_x.Place(x=290,y=60)
+Entry_x.config(bg="white", fg="blue", font=("Times New Roman", 18))
+Entry_x.place(x=290,y=60)
 
 # etiqueta para valor de y 
-lb_y= Label(frame_entrada, text = "Y" =)
+lb_y= Label(frame_entrada, text = "Y" )
+lb_y.config(bg="white", fg="blue", font=("Helvetica", 18))
+lb_y.place(x=290,y=60)
 
 
 #---------------------------------
 # frame operaciones
 #---------------------------------
-frame_azul = Frame(ventana_principal)
-frame_azul.config(bg = "white", width = 480, height = 100)
-frame_azul.place(x = 10, y = 200)
+frame_operaciones = Frame(ventana_principal)
+frame_operaciones.config(bg = "white", width = 480, height = 100)
+frame_operaciones.place(x = 10, y = 200)
+
+# boton para sumar
+bt_sumar = Button(frame_operaciones, text="Sumar")
+bt_sumar.place(x=45, y=35, width=100, height=30)
+
+# boton para borrar
+bt_borrar = Button(frame_operaciones, text="Borrar")
+bt_borrar.place(x=190, y=35, width=100, height=30)
+
+# boton para salir 
+bt_salir = Button(frame_operaciones,text="Salir", command=salir)
+bt_salir.place(x=335, y=35, width=100, height=30)
+
 
 #---------------------------------
 # frame resultados 
@@ -72,6 +88,11 @@ frame_azul.place(x = 10, y = 200)
 frame_rojo = Frame(ventana_principal)
 frame_rojo.config(bg = "white", width = 800, height = 600)
 frame_rojo.place(x = 10, y = 310)
+
+# area de texto para los resultados
+t_resultados = Text(frame_rojo)
+t_resultados.config(bg="black", fg="green", font=("courier, 20"))
+t_resultados.place(x=10, y=10, width=400, height=160)
 
 # run
 # se ejecuta el metodo mainloop de la clase Tk()a traves de la instancia ventana
